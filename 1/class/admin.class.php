f0311e8fa25d79e25a8123a586b4a7ee5d9c7408
degang.huang <degang.huang@yahoo.fr>
2016-06-04 11:36:07 +0000

    test admin.class

--- 1/class/admin.class.php
+++ 1/class/admin.class.php
@@ -3,8 +3,21 @@ class adminUser
 {
 	public function user_login($username,$passowrd,$dbc)
 	{
+		$q = "select * from admin where username ='".$username."' and passowrd='".$passowrd."'";
+
+		$pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname=".SAE_MYSQL_DB, SAE_MYSQL_USER, SAE_MYSQL_PASS);
+		$sth = $pdo->prepare($q);
+		$sth->execute();
+
+
 
 	}
 
 
 }
+
+$admin = new adminUser;
+
+$r = $admin->user_login("degang",md5("123456"));
+
+print_r($r);
