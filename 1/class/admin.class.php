a752a8fa0bb73fbf4fcfad9797b9ddade55323df
degang.huang <degang.huang@yahoo.fr>
2016-06-04 11:37:17 +0000

    test admin.class

--- 1/class/admin.class.php
+++ 1/class/admin.class.php
@@ -4,7 +4,7 @@ class adminUser
 	public function user_login($username,$passowrd)
 	{
 		$q = "select * from admin where username ='".$username."' and passowrd='".$passowrd."'";
-
+		echo $q;
 		$pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname=".SAE_MYSQL_DB, SAE_MYSQL_USER, SAE_MYSQL_PASS);
 		$sth = $pdo->prepare($q);
 		$sth->execute();
