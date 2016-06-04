81015437ed6d39e0e0a92f3c0321f14e1ccb6843
degang.huang <degang.huang@yahoo.fr>
2016-06-04 12:47:45 +0000

    test admin.class

--- 1/class/admin.class.php
+++ 1/class/admin.class.php
@@ -1,13 +1,37 @@
 <?
+class dbConn
+{
+	public $pdo;
+	private static $instance
+
+	private function __construct()
+	{
+		$this->pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname=".SAE_MYSQL_DB, SAE_MYSQL_USER, SAE_MYSQL_PASS);
+	}
+
+	public function dbInit()
+	{
+		if (!isset(self::instance))
+		{
+			$object = __CLASS__;
+			self::instance = new $object;
+		}
+
+		return self::instance;
+	}
+}
+
 class adminUser
 {
 
 
 
-	public function user_login($username,$passowrd,$pdo)
+	public function user_login($username,$passowrd)
 	{
 		$q = "select * from admin where username ='".$username."' and password='".$passowrd."'";
 		//echo $q;
+
+		$pdo = dbConn::dbInit();
 
 		$r = $pdo->query($q);
 		//print_r($r);
@@ -34,6 +58,6 @@ $pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname="
 
 $admin = new adminUser;
 
-$r = $admin->user_login("degang",md5("123456"),$pdo);
+$r = $admin->user_login("degang",md5("123456"));
 
 print_r($r);
