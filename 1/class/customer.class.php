02fdb60a2130ce62a2ee557aa3003fead32cc1c5
degang.huang <degang.huang@yahoo.fr>
2016-06-04 13:24:44 +0000

    cust class

--- 1/class/customer.class.php
+++ 1/class/customer.class.php
@@ -31,4 +31,9 @@ $cust_data = new Customer;
 
 $r = $cust_data->get_customer_by_id(1);
 
+?>
+<html xmlns=http://www.w3.org/1999/xhtml><head>                 <meta http-equiv=Content-Type content="text/html;charset=utf-8">
+
+<?
+
 print_r ($r);
