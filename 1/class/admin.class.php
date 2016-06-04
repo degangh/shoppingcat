782b6318fb0888f8bdbd775a57ab4f694d7a4c29
degang.huang <degang.huang@yahoo.fr>
2016-06-04 13:01:14 +0000

    test admin.class

--- 1/class/admin.class.php
+++ 1/class/admin.class.php
@@ -1,26 +1,4 @@
 <?
-class dbConn
-{
-	public $pdo;
-	private static $instance;
-
-	private function __construct()
-	{
-		$this->pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname=".SAE_MYSQL_DB, SAE_MYSQL_USER, SAE_MYSQL_PASS);
-	}
-
-	public static function dbInit()
-	{
-		if (!isset(self::$instance))
-		{
-			$object = __CLASS__;
-			self::$instance = new $object;
-		}
-
-		return self::$instance;
-	}
-}
-
 class adminUser
 {
 
@@ -53,8 +31,7 @@ class adminUser
 
 }
 
-$pdo = new PDO("mysql:host=".SAE_MYSQL_HOST_M.";port=".SAE_MYSQL_PORT.";dbname=".SAE_MYSQL_DB, SAE_MYSQL_USER, SAE_MYSQL_PASS);
-
+include ("../config/db.php");
 
 $admin = new adminUser;
 
