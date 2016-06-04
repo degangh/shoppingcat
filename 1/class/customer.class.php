842441bd46d2067f8502d0cae98df598eb851fff
degang.huang <degang.huang@yahoo.fr>
2016-06-04 13:11:49 +0000

    cust class

--- 1/class/customer.class.php
+++ 1/class/customer.class.php
@@ -1,9 +1,15 @@
 <?
-class Customer()
+class Customer
 {
 	public function get_customer_by_id($cid)
 	{
-		//
+		$sql = "select * from customer where cid = '".$cid."'";
+
+		$db = dbConn::dbInit();
+
+		$r = $db->pdo->query($sql);
+
+
 	}
 
 	public function update_customer_by_id($cid)
@@ -13,6 +19,14 @@ class Customer()
 
 	public function list_customer($keyword, $page)
 	{
-		
+		//
 	}
 }
+
+include ("../config/db.php");
+
+$cust_data = new Customer;
+
+$r = $cust_data->get_customer_by_id(1);
+
+print_r ($r);
