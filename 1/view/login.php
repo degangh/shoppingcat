a819284bc18c950db94b3d6446de4ac6cb77432e
degang.huang <degang.huang@yahoo.fr>
2016-06-06 13:13:34 +0000

    add view

--- 1/view/login.php
+++ 1/view/login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="../static/bs/css/bootstrap.css" rel="stylesheet">
+	<title>User Login</title>
+	<link href="../static/bs/css/bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
@@ -11,7 +12,7 @@
   		<div class="form-group">
 	    	<label class="control-label col-sm-2" for="email">Email:</label>
 	    	<div class="col-sm-10">
-	      		<input type="email" class="form-control" id="email" placeholder="Enter email">
+	      		<input type="text" class="form-control" id="username" placeholder="Enter User Name">
 	    	</div>
   		</div>
 
