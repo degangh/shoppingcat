847ba77c42194c2eb21443e997de83ca64ad5542
degang.huang <degang.huang@yahoo.fr>
2016-06-06 14:52:51 +0000

    add fetch_assoc

--- 1/class/customer.class.php
+++ 1/class/customer.class.php
@@ -11,14 +11,49 @@ class Customer
 		$r = $db->pdo->query($sql);
 
 
-		return json_encode($r->fetchAll());
+		return json_encode($r->fetchAll(PDO::FETCH_ASSOC));
 
 
 	}
 
-	public function update_customer_by_id($cid)
+	public function update_customer_by_id($cid,$post)
 	{
-		//
+		if (isset($cid))
+		{
+			$op = "update customer set ";
+			$where_clause = "where cid = :cid";
+		}
+
+		else {
+			$op = "insert into customer set ";
+		}
+
+		$data = "cname = :cname, cname_init_py = :cname_init_py, mobile = :mobile, address = :address, postcode = :postcode, id_num = :id_num, comment = :comment";
+
+		$q = $op.$data.$where_clause;
+
+		$db = dbConn::dbInit();
+
+		$stmt = $db->pdo->prepare($q);
+
+		$stmt->bindParam(":cname",$post['cname'],PDO::PARAM_STR);
+		$stmt->bindParam(":cname_init_py",$post['cname_init_py'],PDO::PARAM_STR);
+		$stmt->bindParam(":mobile",$post['mobile'],PDO::PARAM_STR);
+		$stmt->bindParam(":adress",$post['address'],PDO::PARAM_STR);
+		$stmt->bindParam(":postcode",$post['postcode'],PDO::PARAM_STR);
+		$stmt->bindParam(":id_num",$post['id_num'],PDO::PARAM_STR);
+		$stmt->bindParam(":comment",$post['comment'],PDO::PARAM_STR);
+
+		if ($where_class != "")
+		{
+			$stmt->bindParam(":cid", $cid, PARAM_INT);
+		}
+
+		$stmt->execute();
+
+		//TBD
+
+
 	}
 
 	public function list_customer($keyword, $page)
