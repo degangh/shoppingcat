56a6c7c2acc855ab54f5d956fed167cdf4ca8fff
degang.huang <degang.huang@yahoo.fr>
2016-06-08 14:13:06 +0000

    up index

--- 1/index.php
+++ 1/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include_once "config/db.php";
-include_once "class/customer.class.php"
+include_once "class/customer.class.php";
 
 if ($_SESSION['username'] == "") header ("location:view/login.php");
 ?>
@@ -15,7 +15,7 @@ if ($_SESSION['username'] == "") header ("location:view/login.php");
 <body>
 
 <?
-echo "<h3>Hello , Shopping Cat CRM</h3>";
+echo "<h3>Hello ".$_SESSION['username'].", Shopping Cat CRM</h3>";
 ?>
 
 <script src="static/jquery-2.2.4.min.js"></script>
