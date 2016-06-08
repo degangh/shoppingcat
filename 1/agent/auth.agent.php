570f6b3966fee236b4e90a812ea85583d80c12cb
degang.huang <degang.huang@yahoo.fr>
2016-06-08 13:25:35 +0000

    update auth

--- 1/agent/auth.agent.php
+++ 1/agent/auth.agent.php
@@ -5,10 +5,11 @@ include_once "../class/admin.class.php";
 if ($_POST)
 {
 	$admin_user = new adminUser();
-
+	header('Content-Type: application/json');
 	echo $admin_user->user_login($_POST['username'],$_POST['password']);
 }
 
 else {
+	header('Content-Type: application/json');
 	echo json_encode(array("response_code"=>400));# code...
 }
