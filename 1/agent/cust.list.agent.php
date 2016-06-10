d5c3733a948efe42023278cb08bb0614ccc1f62f
degang.huang <degang.huang@yahoo.fr>
2016-06-10 13:12:40 +0000

    up cust

---
+++ 1/agent/cust.list.agent.php
@@ -0,0 +1,22 @@
+<?
+<?
+include_once "../config/db.php";
+include_once "../class/customer.class.php";
+session_start();
+
+header('Content-Type: application/json');
+
+if($_POST)
+{
+	if ($_SESSION['username'])
+	{
+		$customer = new Customer;
+		return $customer->list_customer($keyword,$page);
+	}
+	else {
+		echo json_encode(array("response_code"=>401));# code...
+	}
+}
+else {
+	echo json_encode(array("response_code"=>400));# code...
+}
