55c8f6344313ad15d0287c203225b7d75e1eab83
degang.huang <degang.huang@yahoo.fr>
2016-06-10 12:46:44 +0000

    add cust

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -13,6 +13,7 @@ if ($_SESSION['username'] == "") header ("location:view/login.php");
 </head>
 
 <body>
+<div class='container'>
 	<form role="form" class="form-inline">
 		<div class="form-group">
 			<input type='text' name="keyword" placeholder="Search Key">
@@ -24,6 +25,7 @@ if ($_SESSION['username'] == "") header ("location:view/login.php");
 
 <div>
 
+</div>
 </div>
 
 <script src="../static/jquery-2.2.4.min.js"></script>
