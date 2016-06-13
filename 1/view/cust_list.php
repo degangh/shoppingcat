c69f75481dd50d63be25eebf53212e61ba009038
degang.huang <degang.huang@yahoo.fr>
2016-06-13 12:30:36 +0000

    up cust

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -76,7 +76,7 @@ function processSchData()
 
 				}
 				else {
-					txt  = "<tr class = 'data_row' style='align:center'><td colspan=4>No Result <b>\"" + keyword + "\" </b> !</td></tr>";
+					txt  = "<tr class = 'data_row' style='text-align:center'><td colspan=4>No Result <b>\"" + keyword + "\" </b> !</td></tr>";
 				}
 
 				if (txt != "")
