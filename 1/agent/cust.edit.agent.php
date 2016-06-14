c748831dd87acb698461fd91800992da93c8862e
degang.huang <degang.huang@yahoo.fr>
2016-06-14 13:20:22 +0000

    add cust edit

--- 1/agent/cust.edit.agent.php
+++ 1/agent/cust.edit.agent.php
@@ -12,7 +12,7 @@ if($_POST)
 		$customer = new Customer;
 		$r = $customer->list_customer($cid,$_POST);
 
-		return json_encode(array("response_code"=>200,"dataset"=>$r));
+		echo json_encode(array("response_code"=>200,"dataset"=>$r));
 	}
 	else {
 		echo json_encode(array("response_code"=>401));# code...
