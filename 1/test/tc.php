5efe01c45e9bb667e7ec67125e4cf32ece8fb35c
degang.huang <degang.huang@yahoo.fr>
2016-06-14 13:39:30 +0000

    up tctc.php

--- 1/test/tc.php
+++ 1/test/tc.php
@@ -5,3 +5,7 @@ include_once "../class/customer.class.php";
 $customer = new Customer;
 
 echo $customer->list_customer("hdg","");
+
+$post=array("cnmae"=>"耿玥","cname_init_py"=>"gy","mobile"=>"13436982179");
+
+echo $custome->update_customer_by_id($cid,$post);
