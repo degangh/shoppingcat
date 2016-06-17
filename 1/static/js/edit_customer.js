3211a4648cf66ef0afee8a0c228457c8af419674
degang.huang <degang.huang@yahoo.fr>
2016-06-17 13:24:40 +0000

    add js

--- 1/static/js/edit_customer.js
+++ 1/static/js/edit_customer.js
@@ -68,7 +68,8 @@ $("#save_customer").click(function(event){
 			{
 				alert ("用户保存成功");
 				$("#loading_placeholder").addClass("hidden");
-				window.location.replace("cust_list.php");
+				//window.location.replace("cust_list.php");
+				
 			}
 		}
 	});
