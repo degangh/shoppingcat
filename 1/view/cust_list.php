e270ccce0e7b4572a2e1ccb1e13c6de2ed982a1d
degang.huang <degang.huang@yahoo.fr>
2016-06-18 11:20:29 +0000

    add update cust

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -130,7 +130,7 @@ function processSchData()
 					success: function(data){
 
 						//var cname = data[0].cname;
-						$("#cname").val("xxx";
+						$("#cname").val("xxx");
 
 						openModal();
 					}
