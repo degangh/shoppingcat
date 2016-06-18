a31633e584e3ff250244ea23acd0f5dbff02ac60
degang.huang <degang.huang@yahoo.fr>
2016-06-18 12:00:33 +0000

    test

--- 1/test/tc.php
+++ 1/test/tc.php
@@ -6,6 +6,6 @@ $customer = new Customer;
 
 //echo $customer->list_customer("hdg","");
 
-$post=array("cname"=>"耿玥","cname_init_py"=>"gy","mobile"=>"13436982179","address"=>"somewhere","postcode"=>"100000","id_num"=>"110115");
+$post=array("cid"=>104,"cname"=>"耿玥","cname_init_py"=>"gy","mobile"=>"13436982179","address"=>"somewhere","postcode"=>"100000","id_num"=>"110115");
 
 print_r($customer->update_customer_by_id($cid,$post));
