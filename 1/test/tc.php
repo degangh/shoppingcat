34e1b8f4ed549e9f061296d64573b73b8c3c8929
degang.huang <degang.huang@yahoo.fr>
2016-06-18 12:23:52 +0000

    test

--- 1/test/tc.php
+++ 1/test/tc.php
@@ -13,3 +13,5 @@ if ($_SESSION['username'] == "") header ("location:view/login.php");
 $post=array("cid"=>104,"cname"=>"耿玥","cname_init_py"=>"gy","mobile"=>"13436982179","address"=>"somewhere","postcode"=>"100000","id_num"=>"110115");
 
 print_r($customer->update_customer_by_id($post['cid'],$post));
+
+print_r($customer->update_customer_by_id("",$post));
