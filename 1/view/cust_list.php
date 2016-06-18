0da6f0f88b4f084a8d74cb06472490345389cabb
degang.huang <degang.huang@yahoo.fr>
2016-06-18 11:20:06 +0000

    add update cust

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -129,8 +129,8 @@ function processSchData()
 					data: "cid="+ $(this).data('cid'),
 					success: function(data){
 
-						var cname = data[0].cname;
-						$("#cname").val(cname);
+						//var cname = data[0].cname;
+						$("#cname").val("xxx";
 
 						openModal();
 					}
