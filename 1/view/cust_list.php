a49dca7e3d4038628f834a1f55801b923b35afe1
degang.huang <degang.huang@yahoo.fr>
2016-06-20 13:01:43 +0000

    add popover

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -142,7 +142,7 @@ function processSchData()
 
 				$(".xx1").on("click",function(e)
 				{
-					$(this).popover({content:e.data.cid});
+					$(this).popover({content:$(this).data['cid']});
 					e.preventDefault();
 				});
 
