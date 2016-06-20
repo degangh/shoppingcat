2e12f82a716f496cee693fd1ac6e1ee8281f1eb7
degang.huang <degang.huang@yahoo.fr>
2016-06-20 13:40:02 +0000

    add popover

--- 1/view/cust_list.php
+++ 1/view/cust_list.php
@@ -63,7 +63,7 @@ if ($_SESSION['username'] == "") header ("location:view/login.php");
 	                <h4 class="modal-title">Error 提交错误</h4>
 	            </div>
 				<div class="modal-header">
-	                <button class="close" data-dismiss="modal">&times;</button>
+	                
 	                <h4 class="modal-title"></h4>
 	            </div>
 	            <div class="modal-body">
