76e90908b74f07f18500690a459390d9dafbac57
degang.huang <degang.huang@yahoo.fr>
2016-07-24 09:46:40 +0000

    add tag post

--- 1/static/js/add_tag.js
+++ 1/static/js/add_tag.js
@@ -21,10 +21,14 @@ $("#add_tag_btn").click(function(event){
 
 		var query_str = "cid=" + cid + "&tag_text=" + tag_text;
 
+		$.ajax({type:"POST",
+		url: "../agent/add.tag.agent.php",
+		data: query_str,
+
 		success: function(data){
 			if (data.response_code == 200)
 			{
-				alert ("用户保存成功");
+				alert ("标签保存成功");
 				$("#loading_placeholder").addClass("hidden");
 				//window.location.replace("cust_list.php");
 				$("#genericModal").modal('hide');
@@ -35,6 +39,8 @@ $("#add_tag_btn").click(function(event){
 			}
 		}
 	});
+
+
 
 	}
 
