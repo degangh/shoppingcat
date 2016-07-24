0af85782104fffd17c30376d0dc414235f2f3da1
degang.huang <degang.huang@yahoo.fr>
2016-07-24 05:53:14 +0000

    add tag js

--- 1/static/js/add_tag.js
+++ 1/static/js/add_tag.js
@@ -1,4 +1,8 @@
 $("#add_tag_btn").click(function(event){
-	console.log("clicked");
+	//console.log("clicked");
 	event.preventDefault();
+
+	var cid = $(this).attr("data-cid");
+
+	console.log(cid)
 })
